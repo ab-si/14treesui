16daec3fa72a8965e318e5835123cdb6a06262d0
abhishek.singh <abhishek.singh@arista.com>
2021-02-18 21:20:23 +0530

    Upload google sheet done

--- src/components/UploadData.js
+++ src/components/UploadData.js
@@ -10,6 +10,10 @@ import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Divider from '@material-ui/core/Divider';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import TextField from '@material-ui/core/TextField';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -40,20 +44,70 @@ const useStyles = makeStyles((theme) => ({
     },
     text:{
         paddingTop:'15%',
-        marginLeft:'20%',
+    },
+    tabs:{
+        marginRight: 'auto',
+        marginLeft: 'auto',
+        display:'inline-block'
+    },
+    tab:{
+        marginRight: 'auto',
+        marginLeft: 'auto',
     }
   }));
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`wrapped-tabpanel-${index}`}
+        aria-labelledby={`wrapped-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box p={3}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+  };
+
+  function a11yProps(index) {
+    return {
+      id: `wrapped-tab-${index}`,
+      'aria-controls': `wrapped-tabpanel-${index}`,
+    };
+  }
 
 export default function UploadData() {
     const classes = useStyles();
     const [selectedFile, setSelectedFile] = React.useState(null);
     const [progress, setProgress] = React.useState(0);
+    const [value, setValue] = React.useState('one');
+    const [sheetname, setSheetname] = React.useState('');
+    const handleSheetNameChange = (event) => {
+        setSheetname(event.target.value);
+    };
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
     // On file upload (click the upload button)
     const checkFileSize=(event)=>{
         let file = event.target.files[0]
-        let size = 1500000 // ~ 1500KB 
-        let err = ""; 
+        let size = 1500000 // ~ 1500KB
+        let err = "";
         if (file.size > size) {
             err += file.type+'is too large, please pick a smaller file\n';
         }
@@ -68,7 +122,7 @@ export default function UploadData() {
     const onFileChange = event => {
         console.log(event.target.files)
         var file = event.target.files[0]
-        if(checkFileSize(event)){ 
+        if(checkFileSize(event)){
             // if return true allow to setState
             setSelectedFile(file)
         }
@@ -96,9 +150,8 @@ export default function UploadData() {
          */
         value: PropTypes.number.isRequired,
       };
-    const upload = async () => {
-        console.log(selectedFile)
-        const data = new FormData() 
+    const uploadCsv = async () => {
+        const data = new FormData()
         data.append('file', selectedFile)
         api.post('/api/v1/upload/csv', data, {
             onUploadProgress: progress => {
@@ -108,75 +161,87 @@ export default function UploadData() {
               },
         }).then(data => {
             toast.success('Data successfully uploaded.')
-            console.log(data);
         }).catch(err => {
             toast.error('Error uploading data!!!')
             console.log(err)
         })
     };
+
+    const uploadSheetCsv = async() => {
+        const res = await api.post('/api/v1/upload/googlecsv', {
+            params: {
+                sheetname : sheetname,
+            }
+        });
+        if (res.status===200){
+            toast.success('Data successfully uploaded.')
+        } else {
+            toast.error('Error uploading data!!!')
+        }
+    }
     return (
         <div className={classes.root}>
             <div className="form-group">
                 <ToastContainer />
             </div>
-            <Card className={classes.card}>
-                <div style={{'display':'grid'}}>
-                    <h5 className={classes.text}>Select the csv file for upload</h5>
-                    <input
-                        accept=".csv"
-                        style={{'marginLeft':'25%', 'marginTop': '10%'}}
-                        type="file"
-                        id="contained-button-file"
-                        onChange={onFileChange}
-                    />
-                    <LinearProgressWithLabel value={progress} />
+            <Tabs className={classes.tabs}
+                value={value}
+                indicatorColor="primary"
+                textColor="primary"
+                onChange={handleChange}
+                aria-label="disabled tabs example"
+            >
+                <Tab value="one" label="Upload CSV" {...a11yProps('one')} className={classes.tab}/>
+                <Tab value="two" label="Upload via Google Sheet" {...a11yProps('two')} className={classes.tab}/>
+            </Tabs>
+            <TabPanel value={value} index="one">
+                <Card className={classes.card}>
+                    <div style={{'display':'grid'}}>
+                        <h5 className={classes.text}>Select the csv file for upload</h5>
+                        <input
+                            accept=".csv"
+                            style={{'marginLeft':'25%', 'marginTop': '10%'}}
+                            type="file"
+                            id="contained-button-file"
+                            onChange={onFileChange}
+                        />
+                        <LinearProgressWithLabel value={progress} />
+                        <CardActions className={classes.cardButton}>
+                            <Button
+                                variant="outlined"
+                                color="primary"
+                                component="span"
+                                disabled={!selectedFile}
+                                onClick={uploadCsv}
+                            >
+                            Upload
+                            </Button>
+                        </CardActions>
+                    </div>
+                </Card>
+            </TabPanel>
+            <TabPanel value={value} index="two">
+                <Card className={classes.card}>
+                    <div style={{'display':'block', 'marginTop':'10%'}}>
+                        <h6>1 : Add data to this doc in a new sheet. <a href="https://docs.google.com/spreadsheets/d/1R6CyD_pPLcsah9rlPwJ4dqcLtzkptxJJCrm-dQBu6qM/edit#gid=1637936530" target="_blank" rel="noopener noreferrer">Doc Link</a></h6>
+                        <h6>2 : Make sure to add the data in the given format</h6>
+                        <h6>3 : Give the sheet a name. Enter the name here:</h6>
+                    </div>
+                    <TextField id="standard-name" label="Sheet Name" value={sheetname} onChange={handleSheetNameChange} />
                     <CardActions className={classes.cardButton}>
                         <Button
                             variant="outlined"
                             color="primary"
                             component="span"
-                            disabled={!selectedFile}
-                            onClick={upload}
+                            disabled={!sheetname}
+                            onClick={uploadSheetCsv}
                         >
                         Upload
                         </Button>
                     </CardActions>
-                </div>
-            </Card>
-            {/* <label htmlFor="contained-button-file">
-                <input
-                    accept=".csv"
-                    className={classes.input}
-                    id="contained-button-file"
-                    multiple
-                    type="file"
-                    onChange={(e) => setSelectedFile(e.target.files[0])}
-                />
-                <Button
-                    variant="contained"
-                    color="primary"
-                    size="large"
-                    component="span"
-                    className={classes.button}
-                    startIcon={<ListIcon />}
-                >
-                    Select CSV
-                </Button>
-            </label>
-            <div className="file-name">
-                {selectedFile && selectedFile.length > 0 ? selectedFile[0].name : null}
-            </div>
-            <Button */}
-                {/* variant="contained"
-                color="secondary"
-                size="large"
-                className={classes.button}
-                component="span"
-                disabled={!selectedFile}
-                onClick={upload}
-            >
-                Upload file
-            </Button> */}
+                </Card>
+            </TabPanel>
+            <Divider orientation="vertical" />
         </div>
     )
 }
