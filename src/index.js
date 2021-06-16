c7486eddd83b0a4effb21f621c9ee9ac40e28241
abhishek.singh <abhishek.singh@arista.com>
2021-06-16 21:46:51 +0530

    Rename components to app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './components/Home';
+import Home from './app/Home';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
